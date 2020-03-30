3a76970973983db8835e5d79d02d214c89be4343
Bimal <bimalvpatel14@gmail.com>
2020-03-30 13:53:20 -0400

    First Commit

---
+++ Google Kickstart A 2020/Allocation.py
@@ -0,0 +1,15 @@
+def solve():
+    noofhouses, budget = map(int,input().split())
+    houses = list(map(int,input().split()))
+    houses.sort()
+    i = 0
+    while i < noofhouses and budget >= houses[i]:
+        budget -= houses[i]
+        i += 1
+    return i
+
+
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+        print("Case #"+str(i+1)+": "+str(solve()))
